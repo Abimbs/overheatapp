c499c7cffdbe0c6e21fdb274171db92050433d84
mightyfzeus <bolarinolabisi36@gmail.com>
2022-01-04 19:52:26 -1200

    'cleanup'

--- src/componets/Bookedmodal.js
+++ src/componets/Bookedmodal.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Bookedmodal = ({setModal}) => {
     return (
         <div
-            className="bg-black top-0 left-0 right-0 bottom-0 text-center flex justify-center items-center fixed xl:max-w-lg xl:shadow-lg xl:mx-auto xl:p-10 "
+            className="bg-black top-0 left-0 right-0 px-6 bottom-0 text-center flex justify-center items-center fixed xl:max-w-lg xl:shadow-lg xl:mx-auto xl:p-10 "
             style={{ backgroundColor: "rgba(0,0,0,0.5)" }}
         >
             <div className="bg-white w-96  py-4 rounded-lg shadow-lg mx-auto">
